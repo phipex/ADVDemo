eb9e15a832740cf06ee1059d5b8e758c8ceb6cd9
phipex <phipex@yahoo>
2017-04-18 21:26:44 -0500

    logica de recarga exitosa

--- public/js/controllers/acciones-controller.js
+++ public/js/controllers/acciones-controller.js
@@ -1,25 +1,38 @@
 (function(app) {
-	app.controller('accionesController', ['$scope', '$rootScope', '$state', function($scope,$rootScope, $state) {
+	app.controller('accionesController', ['$scope', '$rootScope', '$state', '$mdDialog', function($scope,$rootScope, $state, $mdDialog) {
 		var acciones = this;
 
 
         acciones.seleccionActual = $rootScope.seleccionActual;
         acciones.onTouch = onTouch;
         acciones.onIngresar = onIngresar;
-        acciones.cedula = null;
+        acciones.recarga = 0;
 
         console.log('loginCabina.seleccionActual',acciones.seleccionActual);
         if(acciones.seleccionActual == null){
             $state.go('seleccion');
         }
 
-        function onIngresar() {
-            //$state.go('acciones');
+        function onIngresar($event) {
+
+            if (acciones.recarga) {
+                var promesa = $mdDialog.show({
+                    contentElement: '#dialogRecargaSuccess',
+                    //parent: angular.element(document.body),
+                    targetEvent: $event,
+                    clickOutsideToClose: true
+                }).finally(function (event) {
+                    console.log(event);
+                    acciones.recarga = 0;
+                });
+            }else{
+
+            }
         }
 
         function onTouch() {
             console.log(acciones.recarga);
-            acciones.recarga = '123456789';
+            acciones.recarga += 10000;
         }
 	}]);
 })(ADVDemo);
